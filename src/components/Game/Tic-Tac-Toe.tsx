6d5a46351f755003e1948a6bb3062a89dda68149
fa-salu <fasalurahman7515@gmail.com>
2025-01-29 22:51:16 +0530

    update winner fucntionality currect update and update style

--- src/components/Game/Tic-Tac-Toe.tsx
+++ src/components/Game/Tic-Tac-Toe.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function TicTacToe() {
   const [board, setBoard] = useState<string[]>(Array(9).fill(""));
   const [xMoveTurn, setXMoveTurn] = useState<boolean>(false);
   const [winner, setWinner] = useState<string | null>(null);
 
-  const checkWinMatch = () => {
+  useEffect(() => {
     const winMatch = [
       [0, 1, 2],
       [3, 4, 5],
@@ -22,43 +22,58 @@ export default function TicTacToe() {
     for (const [a, b, c] of winMatch) {
       if (board[a] && board[a] === board[b] && board[a] === board[c]) {
         setWinner(board[a]);
-        console.log("object", winner);
-        return board[a];
+        return;
       }
     }
-    if (!board.includes("")) {
+
+    if (!board.includes("") && !winner) {
       setWinner("draw");
     }
-  };
+  }, [board, winner]);
 
   const handleClick = (index: number) => {
-    if (board[index]) return;
+    if (board[index] || winner) return;
     const newBoard = [...board];
     newBoard[index] = xMoveTurn ? "X" : "O";
     setBoard(newBoard);
     setXMoveTurn(!xMoveTurn);
-    checkWinMatch();
+  };
+
+  const handlePlay = () => {
+    setBoard(Array(9).fill(""));
+    setWinner(null);
+    setXMoveTurn(false);
   };
 
   return (
-    <div className="flex flex-col h-screen items-center justify-center">
-      <h1 className="text-2xl mb-4">Tic Tac Toe</h1>
+    <div className="flex flex-col h-screen items-center justify-evenly">
+      <h1 className="text-4xl">Tic Tac Toe</h1>
       <div className="grid grid-cols-3 gap-5">
         {board.map((box, index) => (
           <button
             key={index}
             onClick={() => handleClick(index)}
-            className="w-24 h-24 text-2xl font-bold border-2 border-gray-900 rounded"
+            className="w-24 h-24 text-2xl font-bold border-2 border-gray-900 rounded animate-borderMove"
           >
             {box}
           </button>
         ))}
-        {winner && (
-          <div className="text-xl">
-            {winner === "draw" ? "it's draw" : `${winner} wins!`}
-          </div>
-        )}
       </div>
+
+      {winner && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white text-gray-900 text-4xl md:text-6xl font-bold  border-2 animate-borderMove px-10 py-6 rounded-lg shadow-lg">
+            {winner === "draw" ? "It's a draw!" : `${winner} Wins!`}
+          </div>
+        </div>
+      )}
+
+      <button
+        onClick={handlePlay}
+        className="text-2xl z-20 text-black p-3 rounded-xl border border-white animate-borderMove bg-gray-100"
+      >
+        Play Again
+      </button>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,28 @@ export default {
   ],
   theme: {
     extend: {
+      animation: {
+        borderMove: "borderMove 4s linear infinite",
+      },
+      keyframes: {
+        borderMove: {
+          "0%": {
+            borderColor: "#FF0000",
+          },
+          "25%": {
+            borderColor: "#00FF00",
+          },
+          "50%": {
+            borderColor: "#0000FF",
+          },
+          "75%": {
+            borderColor: "#FFFF00",
+          },
+          "100%": {
+            borderColor: "#FF0000",
+          },
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
